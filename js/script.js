57de8642c6b0ac5ab757286d63cb669809c9a5a9
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-01-02 20:48:23 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,69 +6,68 @@ navToggle.addEventListener('click', function () {
   nav.classList.toggle('active');
 });
 
-// ... 
+// API CALL FROM HOME PAGE  
 const baseUrl = "https://tgh.monikalie.no/wp-json/wc/store/products";
+const gamelist = document.querySelector(".games");
 
-const productContainer = document.querySelector(".games");
-// ...
+// Searchfield
 const searchButton = document.querySelector(".search-button");
-// ...
+// Filter
 const categories = document.querySelectorAll(".categories");
 
-function displayProducts(products) {
+function displayGames(games) { 
 document.querySelector(".loading").innerHTML = "";
 
-    products.forEach(function(product) {
-      productContainer.innerHTML += `
-                                    <article class="game">
-                                      <i class="fas fa-heart" id="wishlist"></i>
-                                      <a href="#">
-                                        <img src="${product.images[0].src}" alt="${product.name}"> 
-                                      </a>
-                                      <h3 class="gametitle">${product.name}</h3>
-                                        <p class="price"><b>${product.prices.currency_symbol}</b> ${product.prices.price}</p>
-                                      <button class="buybtn"><i class="fas fa-shopping-cart"></i></button>
-                                    </article>
-                                    `
-    });
+games.forEach(function(game) {
+    gamelist.innerHTML += `
+                            <article class="game">
+                              <i class="fas fa-heart" id="wishlist"></i>
+                                <a href="#">
+                                  <img src="${game.images[0].src}" alt="${game.name}"> 
+                                </a>
+                                <h3 class="gametitle">${game.name}</h3>
+                                  <p class="price"><b>${game.prices.currency_symbol}</b> ${game.prices.price}</p>
+                              <button class="buybtn"><i class="fas fa-shopping-cart"></i></button>
+                            </article>
+                          `
+  });
 }
 
-// REST API ~ call the data 
-async function getProducts(url) {
-    try {
-    const response = await fetch(url);
-    const products = await response.json();
+// REST API - Call the data 
+async function getGameList(url) {
+  try {
+  const response = await fetch(url);
+  const games = await response.json();
 
-    console.log(products);
-    displayProducts(products);
+  displayGames(games);
 
 } catch(error) {
-    productContainer.innerHTML = theError("Oh no!");
-} 
+    gamelist.innerHTML = theError("Oh no!");
+
+  } 
 }
+getGameList(baseUrl);
 
-getProducts(baseUrl);
-
-// Search button ~ should've made an loading animation while typing the button, same with sorting button(s)
+// Search button
 searchButton.onclick = function() {
   const searchInput = document.querySelector("#search-input").value;
   const newUrl = baseUrl + `?search=${searchInput}`;
-  productContainer.innerHTML = "";
-  getProducts(newUrl);
+  gamelist.innerHTML = "";
+  getGameList(newUrl);
 } 
 
-// Categories ~ couldn't make it work on multiple categories or make featured games show up
+// Categories 
 categories.forEach(function(category){
   category.onclick = function(event){
-      let newUrl;
-      if(event.target.id === "featured") {
-          newUrl = baseUrl + "?featured=true";
-      }
-      else{
-          const categoryChosen = event.target.value;
-          newUrl = baseUrl + `?category=${categoryChosen}`
-      }
-      productContainer.innerHTML = ""; 
-      getProducts(newUrl);
+    let newUrl;
+    if(event.target.id === "featured") {
+      newUrl = baseUrl + "?featured=true";
+    }
+    else{
+      const categoryChosen = event.target.value;
+      newUrl = baseUrl + `?category=${categoryChosen}`
+    }
+    gamelist.innerHTML = ""; 
+    getGameList(newUrl);
   }
 });
